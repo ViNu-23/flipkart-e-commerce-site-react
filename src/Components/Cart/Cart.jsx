dc603dc1db2a59149979149881c6666bb601bef0
ViNu-23 <vijayvinu46@gmail.com>
2023-12-23 22:23:01 +0530

    feat:product page,empty cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-
+import './Cart.css'
 export default function Cart() {
   return (
     <>
     <Navbar/>
-     <div>Cart</div>
+     <div className='empty'>
+      <img src='https://rukminim2.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90' alt='empty-cart' className='empty-cart'/>
+     <span>Your cart is empty!. Add items to it now.</span>
+     <button className='w3ba'>Shop Now</button>
+     </div>
+     <div className="footer-cart">
+          <p>&copy; 2023 Your E-Commerce Store. All Rights Reserved.</p>
+        </div>
     </>
   )
 }
--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Product.css';
+import data from '../../DataBase/Data';
+import { Button } from '@chakra-ui/react';
 
 export default function Product() {
+  const dataProducts = data;
+  const [productCounts, setProductCounts] = useState({});
+  const [selectedProductId, setSelectedProductId] = useState(null);
+
+  function buyNow(productId) {
+  }
+
+  function minusOne(productId) {
+    setProductCounts((prevCounts) => ({
+      ...prevCounts,
+      [productId]: Math.max((prevCounts[productId] || 0) - 1, 0),
+    }));
+  }
+
+  function plusOne(productId) {
+    setProductCounts((prevCounts) => ({
+      ...prevCounts,
+      [productId]: (prevCounts[productId] || 0) + 1,
+    }));
+  }
+
   return (
     <>
-    <Navbar/>
-    <div className='container'>
-      <div className='sort'>sort</div>
-    <div className='product-list'>product</div>
-    </div>
+      <Navbar />
+      <div className='store'>
+        <div className='sort-item'></div>
+        <div className='products-store'>
+          {dataProducts.map((item, index) => (
+            <div key={index} id={item.id} className='items-N'>
+              <img src={item.img} alt={item.title} className='products-img' />
+              <span className='product-title'>{item.title}</span>
+              <div className='star'>
+                <div>               
+                   <span className='rating'>{item.star}</span>
+                  <span className='rating'>{item.star}</span>
+                  <span className='rating'>{item.star}</span>
+                  <span className='rating'>{item.star}</span>
+                  </div>
+                <div>
+                <span className='review'>{item.reviews}</span>
+                </div>
+              </div>
+              <div className='price'>
+                <span className='previews-price'>{item.prevPrice}</span>
+                <span className='new-price'> ${item.newPrice}</span>
+              </div>
+
+              <div className='count-item'>
+                <Button onClick={() => minusOne(item.id)} className='cart-value'>
+                  -
+                </Button>
+                {productCounts[item.id] || 0}
+                <Button onClick={() => plusOne(item.id)} className='cart-value'>
+                  +
+                </Button>
+              </div>
+
+              <button
+                type='button'
+                className='btn btn-dark buy-btn'
+                onClick={() => buyNow(item.id)}
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-   
-  )
+  );
 }
