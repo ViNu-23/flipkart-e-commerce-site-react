28fb344724a4c23d07523d626a7b96924bbb95ad
ViNu-23 <vijayvinu46@gmail.com>
2023-12-25 03:12:26 +0530

    feat feature

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import "./Cart.css";
-
+import { Link } from "@chakra-ui/react";
 import { FaTrashAlt } from "react-icons/fa";
 export default function Cart() {
+  const isCartEmpty=0;
   return (
     <>
       <Navbar />
 
-      {/* <div className="empty">
-        <img
-          src="https://rukminim2.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90"
-          alt="empty-cart"
-          className="empty-cart"
-        />
-        <span>Your cart is empty!. Add items to it now.</span>
-        <Link to="/Product" className="nav-links-rm">
-          <button className="w3ba">Shop Now</button>
-        </Link>
-      </div> */}
+{
+  (isCartEmpty &&
+    <div className="empty">
+    <img
+      src="https://rukminim2.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90"
+      alt="empty-cart"
+      className="empty-cart"
+    />
+    <span>Your cart is empty!. Add items to it now.</span>
+    <Link to="/Product" className="nav-links-rm">
+      <button className="w3ba">Shop Now</button>
+    </Link>
+  </div>
+  )
+}
       <div className="cart-container">
         <div className="bought-items">
           <div className="bought-item">
