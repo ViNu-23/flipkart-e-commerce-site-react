0035f9788532e02603d66d833a9b7c344d81190e
ViNu-23 <vijayvinu46@gmail.com>
2024-01-03 23:31:35 +0530

    login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -5,6 +5,8 @@ export default function Login() {
   return (
     <>
     <div className='login-container'>
+<div className='login-img'></div>
+<div className='login-form'></div>
 
     </div>
     </>
